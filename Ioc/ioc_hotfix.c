ad67bf17c81878d9a28543c5546bc114c6ec77a4
Poul-Henning Kamp <phk@FreeBSD.org>
2021-02-21 09:27:11 +0000

    Dont shorten loops quite as hard

--- Ioc/ioc_hotfix.c
+++ Ioc/ioc_hotfix.c
@@ -10,6 +10,8 @@
 #include "ioc.h"
 #include "vend.h"
 
+#define NLOOP	10
+
 void
 Ioc_HotFix_Ioc(uint8_t *eeprom)
 {
@@ -17,31 +19,31 @@ Ioc_HotFix_Ioc(uint8_t *eeprom)
 	 * 80000060 20 3c 00 00 82 35          MOVE.L  #0x00008235,D0
 	 * 80000066 51 c8 ff fe                DBF     D0,0x80000066
 	 */
-	vbe32enc(eeprom + 0x0062, 0x3);
+	vbe32enc(eeprom + 0x0062, NLOOP);
 
 	/*
 	 * 800000fe 20 3c 00 01 04 6a          MOVE.L  #0x0001046a,D0
 	 * 80000104 53 80                      SUBQ.L  #0x1,D0
 	 */
-	vbe32enc(eeprom + 0x0100, 0x3);
+	vbe32enc(eeprom + 0x0100, NLOOP);
 
 	/*
 	 * 80000132 20 3c 00 00 82 35          MOVE.L  #0x00008235,D0
 	 * 80000138 51 c8 ff fe                DBF     D0,0x80000138
 	 */
-	vbe32enc(eeprom + 0x0134, 0x3);
+	vbe32enc(eeprom + 0x0134, NLOOP);
 
 	/*
 	 * 80000338 20 3c 00 00 82 35          MOVE.L  #0x00008235,D0
 	 * 8000033e 51 c8 ff fe                DBF     D0,0x8000033e
 	 */
-	vbe32enc(eeprom + 0x033a, 0x3);
+	vbe32enc(eeprom + 0x033a, NLOOP);
 
 	/*
 	 * 80000348 20 3c 00 00 82 35          MOVE.L  #0x00008235,D0
 	 * 8000034e 51 c8 ff fe                DBF     D0,0x8000034e
 	 */
-	vbe32enc(eeprom + 0x034a, 0x3);
+	vbe32enc(eeprom + 0x034a, NLOOP);
 
 	/*
 	 * 80003822 4e 96                      OUTTEXT
@@ -58,14 +60,14 @@ Ioc_HotFix_Resha(uint8_t *eeprom)
 	 * 0007717c 08 38 00 01 90 01          BTST.B  #0x1,IO_UART_STATUS
 	 * 00077182 67 1e                      BEQ     0x771a2
 	 */
-	vbe32enc(eeprom + 0x7178, 0x3);
+	vbe32enc(eeprom + 0x7178, NLOOP);
 
 	/*
 	 * 00074588 20 3c 00 00 05 00          MOVE.L  #0x00000500,D0
 	 * 0007458e 53 80                      SUBQ.L  #0x1,D0
 	 * 00074590 66 fc                      BNE     0x7458e
 	 */
-	vbe32enc(eeprom + 0x458a, 0x3);
+	vbe32enc(eeprom + 0x458a, NLOOP);
 
 	/*
 	 * 00077386                    SCSI_T_AWAIT_INTERRUPT():
@@ -74,7 +76,7 @@ Ioc_HotFix_Resha(uint8_t *eeprom)
 	 * 0007738e 53 80                      SUBQ.L  #0x1,D0
 	 * 00077390 66 fc                      BNE     0x7738e
 	 */
-	vbe32enc(eeprom + 0x738a, 0x3);
+	vbe32enc(eeprom + 0x738a, NLOOP);
 }
 
 void
@@ -91,9 +93,9 @@ Ioc_HotFix_Bootloader(uint8_t *ram)
 	 * 00054206 51 c9 ff ea                DBF     D1,0x541f2
 	 */
 	if (vbe32dec(ram + 0x541ee) == 0x323c7fff)
-		vbe16enc(ram + 0x541f0, 3);
+		vbe16enc(ram + 0x541f0, NLOOP);
 	if (vbe32dec(ram + 0x541f4) == 0x500)
-		vbe32enc(ram + 0x541f4, 3);
+		vbe32enc(ram + 0x541f4, NLOOP);
 }
 
 static void
@@ -103,7 +105,7 @@ hotfix_kernel_4_2_14(uint8_t *ram)
 	 * 0000648e 20 3c 00 23 7c 4d          MOVE.L  #0x00237c4d,D0
 	 * 00006494 53 80                      SUBQ.L  #0x1,D0
 	 */
-	vbe32enc(ram + 0x6490, 3);
+	vbe32enc(ram + 0x6490, NLOOP);
 
 	/*
 	 * 00009234 20 3c 00 03 ff ff          MOVE.L  #0x0003ffff,D0
@@ -111,21 +113,21 @@ hotfix_kernel_4_2_14(uint8_t *ram)
 	 * 00009242 13 fc 00 00 93 03 ec 18    MOVE.B  #0x00,IO_SCSI_T_18_CMD
 	 * 0000924a 4e b9 00 00 92 10          JSR     0x9210
 	 */
-	vbe32enc(ram + 0x9236, 3);
+	vbe32enc(ram + 0x9236, NLOOP);
 
 	/*
 	 * 00009442 20 3c 00 03 ff ff          MOVE.L  #0x0003ffff,D0
 	 * 00009448 33 fc 00 30 93 03 e0 08    MOVE.W  #0x0030,IO_RESHA_RES_CTL
 	 * 00009450 4e b9 00 00 92 10          JSR     0x9210
 	 */
-	vbe32enc(ram + 0x9444, 3);
+	vbe32enc(ram + 0x9444, NLOOP);
 
 	/*
 	 * 00009512 20 3c 00 07 ff ff          MOVE.L  #0x0007ffff,D0
 	 * 00009518 33 fc 00 30 93 03 e0 08    MOVE.W  #0x0030,IO_RESHA_RES_CTL
 	 * 00009520 4e b9 00 00 92 10          JSR     0x9210
 	 */
-	vbe32enc(ram + 0x9514, 3);
+	vbe32enc(ram + 0x9514, NLOOP);
 
 	/*
 	 * 00009572 30 3c 7f ff                MOVE.W  #0x7fff,D0
@@ -134,7 +136,7 @@ hotfix_kernel_4_2_14(uint8_t *ram)
 	 * 0000957a 30 3c 7f ff                MOVE.W  #0x7fff,D0
 	 * 0000957e 4e b9 00 00 91 ec          JSR     0x91ec
 	 */
-	vbe16enc(ram + 0x9574, 3);
+	vbe16enc(ram + 0x9574, NLOOP);
 
 
 	/*
@@ -143,7 +145,7 @@ hotfix_kernel_4_2_14(uint8_t *ram)
 	 * 00005c5a 66 fc                      BNE 0x5c58
 	 * 00005c5c 51 c9 ff f4                DBF D1,0x5c52
 	 */
-	vbe32enc(ram + 0x5c54, 3);
+	vbe32enc(ram + 0x5c54, NLOOP);
 
 }
 
@@ -160,14 +162,14 @@ hotfix_kernel_4_2_18(uint8_t *ram)
 	 * 00005d24 20 1f                      MOVE.L  (A7)+,D0
 	 * 00005d26 4e 75                      RTS
 	 */
-	vbe32enc(ram + 0x5d18, 3);
+	vbe32enc(ram + 0x5d18, NLOOP);
 
 	/*
 	 * 000066d0 20 3c 00 23 7c 4d     MOVE.L  #0x00237c4d,D0
 	 * 000066d6 53 80                 SUBQ.L  #0x1,D0
 	 * 000066d8 66 fc                 BNE     0x66d6
 	 */
-	vbe32enc(ram + 0x66d2, 3);
+	vbe32enc(ram + 0x66d2, NLOOP);
 
 	/*
 	 * 00009818 20 3c 00 03 ff ff          MOVE.L  #0x0003ffff,D0
@@ -175,7 +177,7 @@ hotfix_kernel_4_2_18(uint8_t *ram)
 	 * 00009826 13 fc 00 00 93 03 ec 18    MOVE.B  #0x00,IO_SCSI_T_18_CMD
 	 * 0000982e 4e b9 00 00 97 f4          JSR     0x97f4
 	 */
-	vbe32enc(ram + 0x981a, 3);
+	vbe32enc(ram + 0x981a, NLOOP);
 
 
 	/*
@@ -183,7 +185,7 @@ hotfix_kernel_4_2_18(uint8_t *ram)
 	 * 00009afc 33 fc 00 30 93 03 e0 08    MOVE.W  #0x0030,IO_RESHA_RES_CTL
 	 * 00009b04 4e b9 00 00 97 f4          JSR     0x97f4
 	 */
-	vbe32enc(ram + 0x9af8, 3);
+	vbe32enc(ram + 0x9af8, NLOOP);
 
 	/*
 	 * 00009b56 30 3c 7f ff                MOVE.W  #0x7fff,D0
@@ -193,7 +195,7 @@ hotfix_kernel_4_2_18(uint8_t *ram)
 	 * 00009b62 4e b9 00 00 97 d0          JSR     0x97d0
 	 * 00009b68 0c 00 00 42                CMPI.B  #0x42,D0
 	 */
-	vbe16enc(ram + 0x9b58, 3);
+	vbe16enc(ram + 0x9b58, NLOOP);
 
 	// XXX: 'tape' make target:
 	// XXX: kernel fails tape initialization
