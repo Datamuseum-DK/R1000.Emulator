2ed38331fbd23c6bf3aae72332657a87697ad259
Poul-Henning Kamp <phk@FreeBSD.org>
2022-01-17 13:36:44 +0000

    Add wiki-table

--- Tests/testsumm.py
+++ Tests/testsumm.py
@@ -6,6 +6,7 @@ class Summarize():
 
         name = test.split("/")[-1]
         self.test = name[1:].replace(".log", "")
+        self.test = self.test.replace("mem32_0_0", "mem32")
 
         print("=" * 72)
         print(self.test.upper())
@@ -35,6 +36,9 @@ class Summarize():
         self.why()
         self.experiments()
 
+    def __lt__(self, other):
+        return self.test < other.test
+
     def missing_chips(self):
         missing = {}
         for flds, line in self.lines:
@@ -51,6 +55,7 @@ class Summarize():
 
     def performance(self):
         silent = True
+        self.stats = []
         for lno, line in enumerate(self.lines):
             if line[1] == "Begin statistics":
                 silent = False
@@ -59,6 +64,7 @@ class Summarize():
             elif line[1] == "End statistics":
                 break
             elif not silent:
+                self.stats.append(line[1])
                 print("\t" + line[1])
         if self.diproc:
             t = int(self.diproc[-1].split()[2]) * 1e-9
@@ -80,8 +86,8 @@ class Summarize():
                 return
 
     def experiments(self):
-        passed = 0
-        failed = 0
+        self.exp_passed = 0
+        self.exp_failed = 0
         hdr = False
         for i in self.console.split(b'\r\n'):
             j = i.split(b'\r\x1b[2K')
@@ -89,7 +95,7 @@ class Summarize():
             if j[-1][-10:] == b'59CPASSED ':
                 final = None
                 k = j[-1].split(b'\r\x1b[59C')
-                passed += len(k) - 1
+                self.exp_passed += len(k) - 1
                 continue
             if j[-1][-10:] == b'69CFAILED ':
                 final = None
@@ -98,22 +104,57 @@ class Summarize():
                     print()
                     print("Failing experiments")
                 k = j[-1].split(b'\x07\r\x1b[69C')
-                failed += len(k) - 1
+                self.exp_failed += len(k) - 1
                 print("\t", k[0].decode("utf-8"))
                 for k in j[:-1]:
                     print("\t\t", k.decode("utf-8"))
                 continue
         print()
-        print("Experiments passed", passed, "failed", failed)
+        print("Experiments ", self.exp_passed + self.exp_failed, "failed", self.exp_failed)
         if final:
             print()
             print("Final Line on Console")
             for k in final:
                 print("\t\t" + str([k]))
 
+def wikitable(tests, file):
+    file.write('{| class="wikitable" style="text-align: right;"\n')
+    file.write("!Test\n")
+    file.write("!Wall Clock\n")
+    file.write("!SystemC\n")
+    file.write("!Ratio\n")
+    file.write("!Exp run\n")
+    file.write("!Exp fail\n")
+    for i in sorted(tests):
+        file.write("|-\n")
+        file.write('| style="text-align: left; |' + i.test + "\n")
+        n = 0
+        for j in i.stats:
+            if "Wall Clock Time" in j:
+                n += 1
+                file.write('|' + "%.3f" % float(j.split()[0]) + "\n")
+            elif "SystemC simulation" in j:
+                n += 1
+                file.write('|' + "%.6f" % float(j.split()[0]) + "\n")
+            elif "SystemC Simulation ratio" in j:
+                n += 1
+                file.write('|' + j.split()[0] + "\n")
+        while n < 3:
+            file.write("|\n")
+            n += 1
+        file.write("|" + str(i.exp_passed + i.exp_failed) + "\n")
+        file.write("|" + str(i.exp_failed) + "\n")
+    file.write("|}\n")
+       
+
+
 def main():
+    tests = []
     for test in sorted(glob.glob("_*.log")):
-        Summarize(test)
+        tests.append(Summarize(test))
+    with open("_wiki", "w") as file:
+        wikitable(tests, file)
 
 if __name__ == "__main__":
     main()
+
