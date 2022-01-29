0077add85025cef3f217adbed305c614c2d72c8d
Poul-Henning Kamp <phk@FreeBSD.org>
2022-01-29 09:31:55 +0000

    Use SC_METHOD where possible

--- Components/F37.cc
+++ Components/F37.cc
@@ -7,18 +7,35 @@
 
 struct scm_f37_state {
 	struct ctx ctx;
+	unsigned dly;
+	bool out;
+	int task;
 };
 
-void
-SCM_F37 :: loadit(const char *arg)
+SCM_F37 :: SCM_F37(sc_module_name nm, const char *arg) : sc_module(nm)
 {
+
 	state = (struct scm_f37_state *)
 	    CTX_Get("f37", this->name(), sizeof *state);
 	should_i_trace(this->name(), &state->ctx.do_trace);
+	state->task = 2;
+	state->dly = 5;
+
+	if (strstr(this->name(), "ECNAN4B") != NULL) {
+		SC_THREAD(doit_t);
+	} else if (strstr(this->name(), "RDNAN0A") != NULL) {
+		SC_METHOD(doit_m);
+		state->dly = 10;
+	} else if (strstr(this->name(), "RDNAN0B") != NULL) {
+		SC_METHOD(doit_m);
+		state->dly = 10;
+	} else {
+		SC_METHOD(doit_m);
+	}
 }
 
 void
-SCM_F37 :: doit(void)
+SCM_F37 :: doit_t(void)
 {
 	while (1) {
 		pin3 = AS(1);
@@ -32,14 +49,51 @@ SCM_F37 :: doit(void)
 			state->ctx.activations++;
 		}
 		if (IS_H(pin1) && IS_H(pin2)) {
-			wait(5, SC_NS);
+			wait(state->dly, SC_NS);
+			state->ctx.activations++;
 			TRACE( << pin1 << pin2 << " 0" );
 			pin3 = AS(0);
 		}
 		if (IS_H(pin1) && IS_H(pin2)) {
 			wait(pin1.negedge_event() | pin2.negedge_event());
 			state->ctx.activations++;
-			wait(5, SC_NS);
+			wait(state->dly, SC_NS);
+		}
+	}
+}
+
+void
+SCM_F37 :: doit_m(void)
+{
+
+	state->ctx.activations++;
+
+	TRACE( << pin1 << pin2 << " " << state->task << " " << state->out);
+	if (state->task == 2) {
+		state->out = IS_L(pin1) || IS_L(pin2);
+		state->task = 1;
+	}
+	if (state->task == 1) {
+		pin3 = AS(state->out);
+		state->task = 0;
+	}
+	if (IS_H(pin1) && IS_H(pin2)) {
+		if (state->out) {
+			state->out = false;
+			state->task = 1;
+			next_trigger(state->dly, SC_NS);
+		} else {
+			next_trigger(pin1.negedge_event() | pin2.negedge_event());
+		}
+	} else {
+		if (!state->out) {
+			state->out = true;
+			state->task = 1;
+			next_trigger(state->dly, SC_NS);
+		} else if(IS_L(pin1)) {
+			next_trigger(pin1.posedge_event());
+		} else {
+			next_trigger(pin2.posedge_event());
 		}
 	}
 }
--- Components/F37.hh
+++ Components/F37.hh
@@ -3,7 +3,6 @@
 
 // (Quad) Two-Input NAND Buffer
 
-
 struct scm_f37_state;
 
 SC_MODULE(SCM_F37)
@@ -14,17 +13,12 @@ SC_MODULE(SCM_F37)
 
 	SC_HAS_PROCESS(SCM_F37);
 
-	SCM_F37(sc_module_name nm, const char *arg) : sc_module(nm)
-	{
-		SC_THREAD(doit);
-		loadit(arg);
-	}
+	SCM_F37(sc_module_name nm, const char *arg);
 
 	private:
 	struct scm_f37_state *state;
-	void loadit(const char *arg);
-
-	void doit(void);
+	void doit_t(void);
+	void doit_m(void);
 };
 
 #endif /* R1000_F37 */
