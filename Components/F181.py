4cfcc027191ff775ca0c00f62ec8e6848be53f8f
Poul-Henning Kamp <phk@FreeBSD.org>
2022-02-09 18:56:24 +0000

    Fix bugs in F181 implementation

--- Components/F181.py
+++ Components/F181.py
@@ -5,61 +5,97 @@ Fairchild DS009491 April 1988 Revised January 2004
 http://www.righto.com/2017/03/inside-vintage-74181-alu-chip-how-it.html
 '''
 
-def stage0(s0, s1, s2, s3, a, b):
-    prop = not (
-        (a)
-        or (b and s0)
-        or (s1 and not b)
-    )
-    gen = not(
-        (not b and s2 and a)
-        or (a and b and s3)
-    )
-    return 0 + prop, 0 + gen
+def ls181(s0, s1, s2, s3, m, cn, a3, a2, a1, a0, b3, b2, b1, b0):
+    mode = str(s0) + str(s1) + str(s2) + str(s3) + "-" + str(m)
+    a = (a3 << 3) | (a2 << 2) | (a1 << 1) | a0;
+    b = (b3 << 3) | (b2 << 2) | (b1 << 1) | b0;
+    cy = 1 - cn
 
-def ls181(cn, m, a3, a2, a1, a0, b3, b2, b1, b0, s3, s2, s1, s0):
-   p0, g0 = stage0(s0, s1, s2, s3, a0, b0)
-   p1, g1 = stage0(s0, s1, s2, s3, a1, b1)
-   p2, g2 = stage0(s0, s1, s2, s3, a2, b2)
-   p3, g3 = stage0(s0, s1, s2, s3, a3, b3)
-   m = not m
-   x1 = not (cn and m)
-   x2 = m and p0
-   x3 = m and g0 and cn
-   x4 = m and p1
-   x5 = m and p0 and g1
-   x6 = m and cn and g0 and g1
-   x7 = m and p2
-   x8 = m and p1 and g2
-   x9 = m and p0 and g1 and g2
-   x10 = m and cn and g1 and g2
-   x11 = not (g0 and g1 and g2 and g3)
-   x12 = not (cn and g0 and g1 and g2 and g3)
-   x13 = p0 and g1 and g2 and g3
-   x14 = p1 and g2 and g3
-   x15 = p2 and g3
-   x16 = p3
-   y1 = p0 ^ g0
-   y2 = not(x2 or x3)
-   y3 = p1 ^ g1
-   y4 = not(x4 or x5 or x6)
-   y5 = p2 ^ g2
-   y6 = not(x7 or x8 or x9 or x10)
-   y7 = p3 ^ g3
-   y8 = not(x13 or x14 or x15 or x16)
-   f0 = x1 ^ y1
-   f1 = y2 ^ y3
-   f2 = y4 ^ y5
-   f3 = y6 ^ y7
-   go = x11
-   cn4 = (not x12) or (not y8)
-   p = y8
-   aeqb = f0 and f1 and f2 and f3
-   return (
-       #p0, g0, p1, g1, p2, g2, p3, g3, '-',
-       #x1 + 0, y1 + 0, y2 + 0, y3 + 0, y4 + 0, y5 + 0, y6 + 0, y7 + 0, x12 + 0, y8 + 0, '-',
-       f3 + 0, f2 + 0, f1 + 0, f0 + 0, aeqb + 0, p + 0, cn4 + 0, go + 0
-   )
+    if mode == "0000-0":
+        f = a + cy
+    elif mode == "0000-1":
+        f = ~a
+    elif mode == "0001-0":
+        f = a + (a & b) + cy
+    elif mode == "0001-1":
+        f = (~a) | b
+    elif mode == "0010-0":
+        f = a + (a & ~b) + cy
+    elif mode == "0010-1":
+        f = (~a) | (~b)
+    elif mode == "0011-0":
+        f = a + a + cy
+    elif mode == "0011-1":
+        f = 15
+    elif mode == "0100-0":
+        f = (a | ~b) + cy
+    elif mode == "0100-1":
+        f = (~a) & b
+    elif mode == "0101-0":
+        f = (a & b) + ((a | ~b) + cy)
+    elif mode == "0101-1":
+        f = b
+    elif mode == "0110-0":
+        f = cy + a - b - 1
+    elif mode == "0110-1":
+        f = a ^ b
+    elif mode == "0111-0":
+        f = a + (a | ~b) + cy
+    elif mode == "0111-1":
+        f = a | b
+    elif mode == "1000-0":
+        f = (a | b) + cy
+    elif mode == "1000-1":
+        f = ~(a | b)
+    elif mode == "1001-0":
+        f = a + b + cy
+    elif mode == "1001-1":
+        f = ~(a ^ b)
+    elif mode == "1010-0":
+        f = (a & ~b) + (a | b) + cy
+    elif mode == "1010-1":
+        f = ~b
+    elif mode == "1011-0":
+        f = a + (a | b) + cy
+    elif mode == "1011-1":
+        f = a | ~b
+    elif mode == "1100-0":
+        f = cy - 1
+    elif mode == "1100-1":
+        f = 0
+    elif mode == "1101-0":
+        f = (a & b) + cy - 1
+    elif mode == "1101-1":
+        f = a & b
+    elif mode == "1110-0":
+        f = (a & ~b) + cy - 1
+    elif mode == "1110-1":
+        f = a & (~b)
+    elif mode == "1111-0":
+        f = a + cy - 1
+    elif mode == "1111-1":
+        f = a
+
+    g0 = not ((s3 and a0 and b0) or (s2 and a0 and not b0))
+    g1 = not ((s3 and a1 and b1) or (s2 and a1 and not b1))
+    g2 = not ((s3 and a2 and b2) or (s2 and a2 and not b2))
+    g3 = not ((s3 and a3 and b3) or (s2 and a3 and not b3))
+    p0 = not (a0 or (s0 and b0) or (s1 and not b0))
+    p1 = not (a1 or (s0 and b1) or (s1 and not b1))
+    p2 = not (a2 or (s0 and b2) or (s1 and not b2))
+    p3 = not (a3 or (s0 and b3) or (s1 and not b3))
+    
+    aeqb = (f & 15) == 15
+    p = (s3 and ((a & b) != 0)) or (s2 and (a & ~b) != 0)
+        
+    g = not (p3 or (g3 and p2) or (g3 and g2 and p1) or (g3 and g2 and g1 and p0))
+        
+    cn4 = (g0 and g1 and g2 and g3 and cn) or (not g)
+
+    f += 16
+    f &= 15
+    return ((f >> 3) & 1, (f >> 2) & 1, (f >> 1) & 1, (f >> 0) & 1, int(aeqb), int(p), int(cn4), int(g))
+
 
 def make_lut():
     for n in range(1 << 14):
@@ -77,7 +113,7 @@ def make_lut():
         s2 = (n >> 2) & 1
         s1 = (n >> 1) & 1
         s0 = (n >> 0) & 1
-        x = ls181(cn, m, a3, a2, a1, a0, b3, b2, b1, b0, s3, s2, s1, s0)
+        x = ls181(s0, s1, s2, s3, m, cn, a3, a2, a1, a0, b3, b2, b1, b0)
         lut = 0
         for i in x:
             lut += lut + i
@@ -86,21 +122,4 @@ def make_lut():
 
 with open("F181_tbl.h", "w") as file:
     for i in zip(*(iter(make_lut()),) * 16):
-        # print(type(i), i)
         file.write(",".join("0x%02x" % x for x in i) + ",\n" )
-
-tbl = []
-for i in make_lut():
-    tbl.append(i)
-# s 0001 m 0 cn 0 a 0000 b 0011 idx 31 f 0100 = 0 p 0 cn 1 g 0
-print(
-    ls181(
-        cn=1,
-        m=1, 
-        a3=1, a2=0, a1=1, a0=0,
-        b3=0, b2=0, b1=1, b0=0, 
-        s3=1, s2=1, s1=0, s0=1,
-    )
-)
-#idx = int("0" + "0" + "11111000" + "1111", 2)
-#print("%x" % idx, bin(256 + tbl[idx])[3:])
