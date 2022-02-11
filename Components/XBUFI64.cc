564c45d3d793460c5e88b80b3d7fbf3d698480a1
Poul-Henning Kamp <phk@FreeBSD.org>
2022-02-11 09:03:04 +0000

    Add 5ns propagation delay

--- Components/XBUFI64.cc
+++ Components/XBUFI64.cc
@@ -5,54 +5,58 @@
 
 struct scm_XBUFI64_state {
 	struct ctx ctx;
-	unsigned slowstart;
-	bool out;
+	int job;
+	bool out[64];
 };
 
 SCM_XBUFI64 :: SCM_XBUFI64(sc_module_name nm, const char *arg) : sc_module(nm)
 {
 	SC_METHOD(doit);
 
-	#define PIN(bit, pin_in, pin_out) \
-	<< pin_in
+	#define PIN(bit, pin_in, pin_out) << pin_in
 	sensitive << pin1 XBUFI64_PINS();
 	#undef PIN
 
 	state = (struct scm_XBUFI64_state *)CTX_Get("XBUFI64", this->name(), sizeof *state);
 	should_i_trace(this->name(), &state->ctx.do_trace);
-	state->slowstart = 100;
 }
 
 void
 SCM_XBUFI64 :: doit(void)
 {
 	state->ctx.activations++;
-	if (state->slowstart) {
-		state->slowstart--;
-		return;
-	}
-	bool z = IS_H(pin1);
+
 
 	#define PIN(bit,pin_in,pin_out) << pin_in
-	if ((state->ctx.do_trace & 2) || !z || state->out)
-		TRACE(<< " e " << pin1 << " d " XBUFI64_PINS());
+	if (state->job || (state->ctx.do_trace & 2)) {
+		TRACE(<< "job " << state->job << " e " << pin1
+		    << " d " XBUFI64_PINS());
+	}
 	#undef PIN
+	if (state->job) {
+		#define PIN(bit,pin_in,pin_out) pin_out = AS(state->out[bit]);
+		XBUFI64_PINS()
+		#undef PIN
+		state->job = 0;
+	}
 
-	if (z) {
-
+	if (IS_H(pin1)) {
 		#define PIN(bit,pin_in,pin_out) \
 		pin_out = sc_logic_Z;
 		XBUFI64_PINS()
 		#undef PIN
-		state->out = false;
-
+		next_trigger(pin1.negedge_event());
 	} else {
-
+		bool tmp;
 		#define PIN(bit,pin_in,pin_out) \
-		pin_out = AS(IS_L(pin_in));
+		tmp = IS_L(pin_in); \
+		if (tmp != state->out[bit]) { \
+			state->job = 1; \
+			state->out[bit] = tmp; \
+		}
 		XBUFI64_PINS()
 		#undef PIN
-		state->out = true;
-
+		if (state->job)
+			next_trigger(5, SC_NS);
 	}
 }
