41859408c3be4f7a3f71b43fbac3219d252da84b
Poul-Henning Kamp <phk@FreeBSD.org>
2022-03-11 19:57:20 +0000

    New XREG16

--- Components/XREG16.cc
+++ Components/XREG16.cc
@@ -1,18 +1,21 @@
 #include <systemc.h>
 #include "Chassis/r1000sc.h"
 #include "Infra/context.h"
+
+#define ANON_PINS
+
 #include "XREG16.hh"
 
 struct scm_xreg16_state {
 	struct ctx ctx;
-	bool reg[64];
+	uint64_t data;
 	int job;
 };
 
 SCM_XREG16 :: SCM_XREG16(sc_module_name nm, const char *arg) : sc_module(nm)
 {
 	SC_METHOD(doit);
-	sensitive << pin1.pos() << pin2;
+	sensitive << PIN_OE << PIN_CLK.pos();
 
 	state = (struct scm_xreg16_state *)CTX_Get("XREG16", this->name(), sizeof *state);
 	should_i_trace(this->name(), &state->ctx.do_trace);
@@ -21,65 +24,6 @@ SCM_XREG16 :: SCM_XREG16(sc_module_name nm, const char *arg) : sc_module(nm)
 void
 SCM_XREG16 :: doit(void)
 {
-	bool nxt[64];
 
-	state->ctx.activations++;
-
-	#define PIN(bit,pin_in,pin_out) \
-	<< state->reg[bit]
-	TRACE(
-	    << " job " << state->job
-	    << " le " << pin1.posedge()
-	    << " oe " << pin2
-	    << " | " XREG16_PINS()
-	);
-	#undef PIN
-
-
-	if (state->job > 0) {
-		#define PIN(bit,pin_in,pin_out) \
-		pin_out = AS(state->reg[bit]);
-		XREG16_PINS()
-		#undef PIN
-		state->job = 0;
-	} else if (state->job < 0) {
-		#define PIN(bit,pin_in,pin_out) \
-		pin_out = sc_logic_Z;
-		XREG16_PINS()
-		#undef PIN
-	}
-
-	if (pin1.posedge()) {
-		if (IS_H(pin2)) {
-			#define PIN(bit,pin_in,pin_out) \
-			state->reg[bit] = IS_H(pin_in);
-			XREG16_PINS()
-			#undef PIN
-		} else {
-
-			#define PIN(bit,pin_in,pin_out) \
-			nxt[bit] = IS_H(pin_in); \
-			if (nxt[bit] != state->reg[bit]) state->job = 1;
-			XREG16_PINS()
-			#undef PIN
-
-			if (state->job != 0) {
-				state->job = 1;
-				#define PIN(bit,pin_in,pin_out) \
-				state->reg[bit] = nxt[bit];
-				XREG16_PINS()
-				next_trigger(5, SC_NS);
-			}
-		}
-	}
-
-	if (IS_H(pin2)) {
-		if (state->job >= 0) {
-			state->job = -1;
-			next_trigger(5, SC_NS);
-		}
-	} else if (state->job != 0) {
-		state->job = 1;
-		next_trigger(5, SC_NS);
-	}
+	#include "XREG_doit.hh"
 }
--- Components/XREG16.hh
+++ Components/XREG16.hh
@@ -1,31 +1,14 @@
 #ifndef R1000_XREG16
 #define R1000_XREG16
 
+#include "XREG16_pins.hh"
+
 struct scm_xreg16_state;
-
-#define XREG16_PINS() \
-	PIN(0, pin3, pin19) PIN(1, pin4, pin20) PIN(2, pin5, pin21) \
-	PIN(3, pin6, pin22) PIN(4, pin7, pin23) PIN(5, pin8, pin24) \
-	PIN(6, pin9, pin25) PIN(7, pin10, pin26) PIN(8, pin11, pin27) \
-	PIN(9, pin12, pin28) PIN(10, pin13, pin29) PIN(11, pin14, pin30) \
-	PIN(12, pin15, pin31) PIN(13, pin16, pin32) PIN(14, pin17, pin33) \
-	PIN(15, pin18, pin34)
-
 
 SC_MODULE(SCM_XREG16)
 {
-	sc_in <sc_logic>	pin1;	// CLK_ (input)
-	sc_in <sc_logic>	pin2;	// OE_ (input)
 
-	#define PIN(bit, pin_in, pin_out) \
-	sc_in <sc_logic>	pin_in;
-	XREG16_PINS()
-	#undef PIN
-
-	#define PIN(bit, pin_in, pin_out) \
-	sc_out <sc_logic>	pin_out;
-	XREG16_PINS()
-	#undef PIN
+	XREG16_PINLIST
 
 	SC_HAS_PROCESS(SCM_XREG16);
 
