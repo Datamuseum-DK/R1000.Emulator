c5245865ff23415d0bbe40c8f6ce26e3d5eaf3f8
Poul-Henning Kamp <phk@FreeBSD.org>
2022-03-12 13:26:10 +0000

    Update XSR16

--- Components/XSR16.cc
+++ Components/XSR16.cc
@@ -1,6 +1,8 @@
 #include <systemc.h>
 #include "Chassis/r1000sc.h"
 #include "Infra/context.h"
+
+#define ANON_PINS
 #include "XSR16.hh"
 
 // tripple 4-bit bidirectional universal shift register
@@ -16,7 +18,7 @@ SCM_XSR16 :: SCM_XSR16(sc_module_name nm, const char *arg) : sc_module(nm)
 {
 	(void)arg;
 	SC_METHOD(doit);
-	sensitive << pin2 << pin1.pos();
+	sensitive << XSR16_PIN_CLR << XSR16_PIN_CLK.pos();
 
 	state = (struct scm_xsr16_state *)
 	    CTX_Get("xsr16", this->name(), sizeof *state);
@@ -29,70 +31,77 @@ void
 SCM_XSR16 :: doit(void)
 {
 	const char *what = NULL;
-	unsigned nxt;
+	unsigned nxt, mode = 0;
 
 	state->ctx.activations++;
 	if (state->job) {
-		pin23 = AS(state->out & (1<<15));
-		pin24 = AS(state->out & (1<<14));
-		pin25 = AS(state->out & (1<<13));
-		pin26 = AS(state->out & (1<<12));
-		pin27 = AS(state->out & (1<<11));
-		pin28 = AS(state->out & (1<<10));
-		pin29 = AS(state->out & (1<<9));
-		pin30 = AS(state->out & (1<<8));
-		pin31 = AS(state->out & (1<<7));
-		pin32 = AS(state->out & (1<<6));
-		pin33 = AS(state->out & (1<<5));
-		pin34 = AS(state->out & (1<<4));
-		pin35 = AS(state->out & (1<<3));
-		pin36 = AS(state->out & (1<<2));
-		pin37 = AS(state->out & (1<<1));
-		pin38 = AS(state->out & (1<<0));
+		PIN_Q0 = AS(state->out & (1<<15));
+		PIN_Q1 = AS(state->out & (1<<14));
+		PIN_Q2 = AS(state->out & (1<<13));
+		PIN_Q3 = AS(state->out & (1<<12));
+		PIN_Q4 = AS(state->out & (1<<11));
+		PIN_Q5 = AS(state->out & (1<<10));
+		PIN_Q6 = AS(state->out & (1<<9));
+		PIN_Q7 = AS(state->out & (1<<8));
+		PIN_Q8 = AS(state->out & (1<<7));
+		PIN_Q9 = AS(state->out & (1<<6));
+		PIN_Q10 = AS(state->out & (1<<5));
+		PIN_Q11 = AS(state->out & (1<<4));
+		PIN_Q12 = AS(state->out & (1<<3));
+		PIN_Q13 = AS(state->out & (1<<2));
+		PIN_Q14 = AS(state->out & (1<<1));
+		PIN_Q15 = AS(state->out & (1<<0));
 		state->job = 0;
 	}
 	nxt = state->out;
-	if (IS_L(pin2)) {
+	if (IS_L(PIN_CLR)) {
 		what = " clr ";
 		if (nxt)
 			nxt = 0;
 		else
-			next_trigger(pin1.posedge_event());
-	} else if (pin1.posedge()) {
-		if (IS_H(pin5) && IS_H(pin6)) {
+			next_trigger(PIN_CLR.posedge_event());
+	} else if (PIN_CLK.posedge()) {
+		if (IS_H(PIN_S0)) mode |= 2;
+		if (IS_H(PIN_S1)) mode |= 1;
+		switch (mode) {
+		case 3:
 			what = " load ";
 			nxt = 0;
-			if (IS_H(pin7)) nxt |= (1<<15);
-			if (IS_H(pin8)) nxt |= (1<<14);
-			if (IS_H(pin9)) nxt |= (1<<13);
-			if (IS_H(pin10)) nxt |= (1<<12);
-			if (IS_H(pin11)) nxt |= (1<<11);
-			if (IS_H(pin12)) nxt |= (1<<10);
-			if (IS_H(pin13)) nxt |= (1<<9);
-			if (IS_H(pin14)) nxt |= (1<<8);
-			if (IS_H(pin15)) nxt |= (1<<7);
-			if (IS_H(pin16)) nxt |= (1<<6);
-			if (IS_H(pin17)) nxt |= (1<<5);
-			if (IS_H(pin18)) nxt |= (1<<4);
-			if (IS_H(pin19)) nxt |= (1<<3);
-			if (IS_H(pin20)) nxt |= (1<<2);
-			if (IS_H(pin21)) nxt |= (1<<1);
-			if (IS_H(pin22)) nxt |= (1<<0);
-		} else if (IS_L(pin6) && IS_H(pin5)) {
-			what = " right ";
+			if (IS_H(PIN_D0)) nxt |= (1<<15);
+			if (IS_H(PIN_D1)) nxt |= (1<<14);
+			if (IS_H(PIN_D2)) nxt |= (1<<13);
+			if (IS_H(PIN_D3)) nxt |= (1<<12);
+			if (IS_H(PIN_D4)) nxt |= (1<<11);
+			if (IS_H(PIN_D5)) nxt |= (1<<10);
+			if (IS_H(PIN_D6)) nxt |= (1<<9);
+			if (IS_H(PIN_D7)) nxt |= (1<<8);
+			if (IS_H(PIN_D8)) nxt |= (1<<7);
+			if (IS_H(PIN_D9)) nxt |= (1<<6);
+			if (IS_H(PIN_D10)) nxt |= (1<<5);
+			if (IS_H(PIN_D11)) nxt |= (1<<4);
+			if (IS_H(PIN_D12)) nxt |= (1<<3);
+			if (IS_H(PIN_D13)) nxt |= (1<<2);
+			if (IS_H(PIN_D14)) nxt |= (1<<1);
+			if (IS_H(PIN_D15)) nxt |= (1<<0);
+			break;
+		case 2:
+			what = " >> ";
 			nxt >>= 1;
 			if (IS_H(pin3)) nxt |= (1<<15);
-		} else if (IS_H(pin6) && IS_L(pin5)) {
-			what = " left ";
+			break;
+		case 1:
+			what = " << ";
 			nxt <<= 1;
 			nxt &= 0xffff;
 			if (IS_H(pin4)) nxt |= (1<<0);
-		} else {
+			break;
+		case 0:
 			next_trigger(
-			    pin1.negedge_event() |
-			    pin10.posedge_event() |
-			    pin9.posedge_event()
+			    PIN_CLR.negedge_event() |
+			    PIN_S0.posedge_event() |
+			    PIN_S1.posedge_event()
 			);
+			break;
 		}
 	}
 	if ((state->ctx.do_trace & 2) && what == NULL)
@@ -103,22 +112,22 @@ SCM_XSR16 :: doit(void)
 		    << " mr_ " << pin2 // MR_
 		    << " dsr " << pin3 // DSR
 		    << " d "
-		    << pin7
-		    << pin8
-		    << pin9
-		    << pin10
-		    << pin11
-		    << pin12
-		    << pin13
-		    << pin14
-		    << pin15
-		    << pin16
-		    << pin17
-		    << pin18
-		    << pin19
-		    << pin20
-		    << pin21
-		    << pin22
+		    << PIN_D0
+		    << PIN_D1
+		    << PIN_D2
+		    << PIN_D3
+		    << PIN_D4
+		    << PIN_D5
+		    << PIN_D6
+		    << PIN_D7
+		    << PIN_D8
+		    << PIN_D9
+		    << PIN_D10
+		    << PIN_D11
+		    << PIN_D12
+		    << PIN_D13
+		    << PIN_D14
+		    << PIN_D15
 		    << " dsl " << pin4 // DSL
 		    << " s " << pin5 << pin6
 		    << " cp " << pin1 // CP
--- Components/XSR16.hh
+++ Components/XSR16.hh
@@ -1,6 +1,8 @@
 #ifndef R1000_XSR16
 #define R1000_XSR16
 
+#include "XSR16_pins.hh"
+
 // (quad) 4-bit bidirectional universal shift register
 // Philips 1989 Apr 04, IC15 Data Handbook
 
@@ -8,45 +10,45 @@ struct scm_xsr16_state;
 
 SC_MODULE(SCM_XSR16)
 {
-	sc_in <sc_logic>	pin1;	// clk
-	sc_in <sc_logic>	pin2;	// clr
-	sc_in <sc_logic>	pin3;	// rsi
-	sc_in <sc_logic>	pin4;	// lsi
-	sc_in <sc_logic>	pin5;	// s0
-	sc_in <sc_logic>	pin6;	// s1
-	sc_in <sc_logic>	pin7;	// d0
-	sc_in <sc_logic>	pin8;	// d1
-	sc_in <sc_logic>	pin9;	// d2
-	sc_in <sc_logic>	pin10;	// d3
-	sc_in <sc_logic>	pin11;	// d4
-	sc_in <sc_logic>	pin12;	// d5
-	sc_in <sc_logic>	pin13;	// d6
-	sc_in <sc_logic>	pin14;	// d7
-	sc_in <sc_logic>	pin15;	// d8
-	sc_in <sc_logic>	pin16;	// d9
-	sc_in <sc_logic>	pin17;	// d10
-	sc_in <sc_logic>	pin18;	// d11
-	sc_in <sc_logic>	pin19;	// d12
-	sc_in <sc_logic>	pin20;	// d13
-	sc_in <sc_logic>	pin21;	// d14
-	sc_in <sc_logic>	pin22;	// d15
+	sc_in <sc_logic>	XSR16_PIN_CLK;
+	sc_in <sc_logic>	XSR16_PIN_CLR;
+	sc_in <sc_logic>	XSR16_PIN_RSI;
+	sc_in <sc_logic>	XSR16_PIN_LSI;
+	sc_in <sc_logic>	XSR16_PIN_S0;
+	sc_in <sc_logic>	XSR16_PIN_S1;
+	sc_in <sc_logic>	XSR16_PIN_D0;
+	sc_in <sc_logic>	XSR16_PIN_D1;
+	sc_in <sc_logic>	XSR16_PIN_D2;
+	sc_in <sc_logic>	XSR16_PIN_D3;
+	sc_in <sc_logic>	XSR16_PIN_D4;
+	sc_in <sc_logic>	XSR16_PIN_D5;
+	sc_in <sc_logic>	XSR16_PIN_D6;
+	sc_in <sc_logic>	XSR16_PIN_D7;
+	sc_in <sc_logic>	XSR16_PIN_D8;
+	sc_in <sc_logic>	XSR16_PIN_D9;
+	sc_in <sc_logic>	XSR16_PIN_D10;
+	sc_in <sc_logic>	XSR16_PIN_D11;
+	sc_in <sc_logic>	XSR16_PIN_D12;
+	sc_in <sc_logic>	XSR16_PIN_D13;
+	sc_in <sc_logic>	XSR16_PIN_D14;
+	sc_in <sc_logic>	XSR16_PIN_D15;
 
-	sc_out <sc_logic>	pin23;	// q0
-	sc_out <sc_logic>	pin24;	// q1
-	sc_out <sc_logic>	pin25;	// q2
-	sc_out <sc_logic>	pin26;	// q3
-	sc_out <sc_logic>	pin27;	// q4
-	sc_out <sc_logic>	pin28;	// q5
-	sc_out <sc_logic>	pin29;	// q6
-	sc_out <sc_logic>	pin30;	// q7
-	sc_out <sc_logic>	pin31;	// q8
-	sc_out <sc_logic>	pin32;	// q9
-	sc_out <sc_logic>	pin33;	// q10
-	sc_out <sc_logic>	pin34;	// q11
-	sc_out <sc_logic>	pin35;	// q12
-	sc_out <sc_logic>	pin36;	// q13
-	sc_out <sc_logic>	pin37;	// q14
-	sc_out <sc_logic>	pin38;	// q15
+	sc_out <sc_logic>	XSR16_PIN_Q0;
+	sc_out <sc_logic>	XSR16_PIN_Q1;
+	sc_out <sc_logic>	XSR16_PIN_Q2;
+	sc_out <sc_logic>	XSR16_PIN_Q3;
+	sc_out <sc_logic>	XSR16_PIN_Q4;
+	sc_out <sc_logic>	XSR16_PIN_Q5;
+	sc_out <sc_logic>	XSR16_PIN_Q6;
+	sc_out <sc_logic>	XSR16_PIN_Q7;
+	sc_out <sc_logic>	XSR16_PIN_Q8;
+	sc_out <sc_logic>	XSR16_PIN_Q9;
+	sc_out <sc_logic>	XSR16_PIN_Q10;
+	sc_out <sc_logic>	XSR16_PIN_Q11;
+	sc_out <sc_logic>	XSR16_PIN_Q12;
+	sc_out <sc_logic>	XSR16_PIN_Q13;
+	sc_out <sc_logic>	XSR16_PIN_Q14;
+	sc_out <sc_logic>	XSR16_PIN_Q15;
 
 	SC_HAS_PROCESS(SCM_XSR16);
 
