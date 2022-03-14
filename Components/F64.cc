a40fe8641ca92a494f1be2e8b1c2feff43a50cfa
Poul-Henning Kamp <phk@FreeBSD.org>
2022-03-14 10:04:45 +0000

    Decompose F64 with a AND sub-class, because the CKAOI* often have high-speed, but seldomly used, clock inputs.
    No similar benefit seen for F51, which so often have parallel
    connections between inputs, that it runs slower of decomposed.

--- Components/F64.cc
+++ Components/F64.cc
@@ -6,42 +6,119 @@
 // 4-2-3-2-Input AND-OR-Invert Gate
 // Fairchild DS009467 April 1988 Revised March 1999
 
+SC_MODULE(SCM_F64_2)
+{
+	sc_in <sc_logic>	term1;
+	sc_in <sc_logic>	term2;
+	sc_in <sc_logic>	term3;
+	sc_in <sc_logic>	term4;
+	sc_out <bool>		term5;
+
+	SC_HAS_PROCESS(SCM_F64_2);
+
+	SCM_F64_2(sc_module_name nm, uint64_t *up_act_cnt) : sc_module(nm)
+	{
+		act_cnt = up_act_cnt;
+		SC_METHOD(doit);
+		sensitive << term1 << term2 << term3 << term4;
+	}
+
+	void
+	doit(void)
+	{
+		(*act_cnt)++;
+		term5 = IS_H(term1) && IS_H(term2) && IS_H(term3) && IS_H(term4);
+		if (term1 != sc_logic_1)
+			next_trigger(term1.posedge_event());
+		else if (term2 != sc_logic_1)
+			next_trigger(term2.posedge_event());
+		else if (term3 != sc_logic_1)
+			next_trigger(term3.posedge_event());
+		else if (term4 != sc_logic_1)
+			next_trigger(term4.posedge_event());
+	}
+
+	uint64_t *act_cnt;
+};
+
 struct scm_f64_state {
 	struct ctx ctx;
+	SCM_F64_2 *input_a;
+	SCM_F64_2 *input_b;
+	SCM_F64_2 *input_c;
+	SCM_F64_2 *input_d;
+	sc_signal<bool> *sig_a;
+	sc_signal<bool> *sig_b;
+	sc_signal<bool> *sig_c;
+	sc_signal<bool> *sig_d;
 };
 
 SCM_F64 :: SCM_F64(sc_module_name nm, const char *arg) : sc_module(nm)
 {
-	SC_METHOD(doit);
-	sensitive << pin1 << pin2 << pin3 << pin4 << pin5 << pin6
-		  << pin9 << pin10 << pin11 << pin12 << pin13;
-
 	state = (struct scm_f64_state *)
 	    CTX_Get("f64", this->name(), sizeof *state);
 	should_i_trace(this->name(), &state->ctx.do_trace);
+
+	state->sig_a = new sc_signal<bool>("sig_a");
+	state->sig_b = new sc_signal<bool>("sig_b");
+	state->sig_c = new sc_signal<bool>("sig_c");
+	state->sig_d = new sc_signal<bool>("sig_d");
+
+	state->input_a = new SCM_F64_2("siga", &state->ctx.activations);
+	state->input_a->term1(pin2);
+	state->input_a->term2(pin3);
+	state->input_a->term3(pin3);
+	state->input_a->term4(pin3);
+	state->input_a->term5(*(state->sig_a));
+
+	state->input_b = new SCM_F64_2("sigb", &state->ctx.activations);
+	state->input_b->term1(pin9);
+	state->input_b->term2(pin10);
+	state->input_b->term3(pin10);
+	state->input_b->term4(pin10);
+	state->input_b->term5(*(state->sig_b));
+
+	state->input_c = new SCM_F64_2("sigc", &state->ctx.activations);
+	state->input_c->term1(pin4);
+	state->input_c->term2(pin5);
+	state->input_c->term3(pin6);
+	state->input_c->term4(pin6);
+	state->input_c->term5(*(state->sig_c));
+
+	state->input_d = new SCM_F64_2("sigd", &state->ctx.activations);
+	state->input_d->term1(pin1);
+	state->input_d->term2(pin13);
+	state->input_d->term3(pin12);
+	state->input_d->term4(pin11);
+	state->input_d->term5(*(state->sig_d));
+
+	SC_METHOD(doit);
+	sensitive << *(state->sig_a) << *(state->sig_b) << *(state->sig_c) << *(state->sig_d);
 }
 
 void
 SCM_F64 :: doit(void)
 {
-	state->ctx.activations++;
-	bool s = !(
-	    (IS_H(pin2) && IS_H(pin3))
-	    || (IS_H(pin1) && IS_H(pin13) && IS_H(pin12) && IS_H(pin11))
-	    || (IS_H(pin4) && IS_H(pin5) && IS_H(pin6))
-	    || (IS_H(pin9) && IS_H(pin10))
-	);
+	bool a = *state->sig_a;
+	bool b = *state->sig_b;
+	bool c = *state->sig_c;
+	bool d = *state->sig_d;
+	bool s = !(a || b || c || d);
 	TRACE(
+	    << a << "="
 	    << pin2
 	    << pin3
 	    << ','
+	    << b << "="
 	    << pin9
 	    << pin10
 	    << ','
+	    << c << "="
 	    << pin4
 	    << pin5
 	    << pin6
 	    << ','
+	    << d << "="
 	    << pin1
 	    << pin11
 	    << pin12
