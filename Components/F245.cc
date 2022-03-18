b8c2b9276918300257ff1050859f3d3bf387ef67
Poul-Henning Kamp <phk@FreeBSD.org>
2022-03-18 18:20:34 +0000

    Split F245 in two symmetric halves to reduce pin sensitivity

--- Components/F245.cc
+++ Components/F245.cc
@@ -6,97 +6,110 @@
 // Octal Bidirectional Transceiver with 3-STATE Outputs
 // Fairchild Rev 1.4.0 January 2008
 
+SC_MODULE(SCM_F245_5)
+{
+	sc_in <sc_logic>	pin_oe;
+	sc_in <sc_logic>	pin_dir;
+	sc_in <sc_logic>	pin_d0;
+	sc_in <sc_logic>	pin_d1;
+	sc_in <sc_logic>	pin_d2;
+	sc_in <sc_logic>	pin_d3;
+	sc_in <sc_logic>	pin_d4;
+	sc_in <sc_logic>	pin_d5;
+	sc_in <sc_logic>	pin_d6;
+	sc_in <sc_logic>	pin_d7;
+	sc_out <sc_logic>	pin_y0;
+	sc_out <sc_logic>	pin_y1;
+	sc_out <sc_logic>	pin_y2;
+	sc_out <sc_logic>	pin_y3;
+	sc_out <sc_logic>	pin_y4;
+	sc_out <sc_logic>	pin_y5;
+	sc_out <sc_logic>	pin_y6;
+	sc_out <sc_logic>	pin_y7;
+
+	SC_HAS_PROCESS(SCM_F245_5);
+
+	SCM_F245_5(sc_module_name nm, uint64_t *up_act_cnt, bool dir) :
+	    sc_module(nm)
+	{
+		act_cnt = up_act_cnt;
+		mydir = dir;
+		SC_METHOD(doit);
+		sensitive << pin_oe << pin_dir << pin_d0 << pin_d1 <<
+		    pin_d2 << pin_d3 << pin_d4 << pin_d5 << pin_d6 << pin_d7;
+	}
+
+	uint64_t *act_cnt;
+	bool mydir;
+
+	void
+	doit(void)
+	{
+		(*act_cnt)++;
+		if (IS_H(pin_oe)) {
+			pin_y0 = sc_logic_Z;
+			pin_y1 = sc_logic_Z;
+			pin_y2 = sc_logic_Z;
+			pin_y3 = sc_logic_Z;
+			pin_y4 = sc_logic_Z;
+			pin_y5 = sc_logic_Z;
+			pin_y6 = sc_logic_Z;
+			pin_y7 = sc_logic_Z;
+			next_trigger(pin_oe.negedge_event());
+		} else if (mydir ^ IS_H(pin_dir)) {
+			pin_y0 = sc_logic_Z;
+			pin_y1 = sc_logic_Z;
+			pin_y2 = sc_logic_Z;
+			pin_y3 = sc_logic_Z;
+			pin_y4 = sc_logic_Z;
+			pin_y5 = sc_logic_Z;
+			pin_y6 = sc_logic_Z;
+			pin_y7 = sc_logic_Z;
+			next_trigger(pin_dir.default_event());
+		} else {
+			pin_y0 = pin_d0;
+			pin_y1 = pin_d1;
+			pin_y2 = pin_d2;
+			pin_y3 = pin_d3;
+			pin_y4 = pin_d4;
+			pin_y5 = pin_d5;
+			pin_y6 = pin_d6;
+			pin_y7 = pin_d7;
+		}
+	}
+};
+
 struct scm_f245_state {
 	struct ctx ctx;
+	SCM_F245_5 *ab;
+	SCM_F245_5 *ba;
 };
 
 SCM_F245 :: SCM_F245(sc_module_name nm, const char *arg) : sc_module(nm)
 {
-	SC_METHOD(doit);
-	sensitive << pin1 << pin2 << pin3 << pin4 << pin5 << pin6
-		  << pin7 << pin8 << pin9 << pin11 << pin12 << pin13
-		  << pin14 << pin15 << pin16 << pin17 << pin18 << pin19;
 
 	state = (struct scm_f245_state *)
 	    CTX_Get("f245", this->name(), sizeof *state);
 	should_i_trace(this->name(), &state->ctx.do_trace);
-}
 
-void
-SCM_F245 :: doit(void)
-{
-	state->ctx.activations++;
-	TRACE(
-	    << " dir " << pin1
-	    << " oe_ " << pin19
-	    << " a " << pin2
-	    <<pin3
-	    <<pin4
-	    <<pin5
-	    <<pin6
-	    <<pin7
-	    <<pin8
-	    <<pin9
-	    << " b " << pin18
-	    <<pin17
-	    <<pin16
-	    <<pin15
-	    <<pin14
-	    <<pin13
-	    <<pin12
-	    <<pin11
-	);
-	if (IS_H(pin19)) {
-		pin2 = sc_logic_Z;
-		pin3 = sc_logic_Z;
-		pin4 = sc_logic_Z;
-		pin5 = sc_logic_Z;
-		pin6 = sc_logic_Z;
-		pin7 = sc_logic_Z;
-		pin8 = sc_logic_Z;
-		pin9 = sc_logic_Z;
-		pin11 = sc_logic_Z;
-		pin12 = sc_logic_Z;
-		pin13 = sc_logic_Z;
-		pin14 = sc_logic_Z;
-		pin15 = sc_logic_Z;
-		pin16 = sc_logic_Z;
-		pin17 = sc_logic_Z;
-		pin18 = sc_logic_Z;
-		next_trigger(pin19.negedge_event());
-	} else if (IS_L(pin1)) {
-		pin11 = sc_logic_Z;
-		pin12 = sc_logic_Z;
-		pin13 = sc_logic_Z;
-		pin14 = sc_logic_Z;
-		pin15 = sc_logic_Z;
-		pin16 = sc_logic_Z;
-		pin17 = sc_logic_Z;
-		pin18 = sc_logic_Z;
-		pin2 = AS(IS_H(pin18));
-		pin3 = AS(IS_H(pin17));
-		pin4 = AS(IS_H(pin16));
-		pin5 = AS(IS_H(pin15));
-		pin6 = AS(IS_H(pin14));
-		pin7 = AS(IS_H(pin13));
-		pin8 = AS(IS_H(pin12));
-		pin9 = AS(IS_H(pin11));
-	} else {
-		pin2 = sc_logic_Z;
-		pin3 = sc_logic_Z;
-		pin4 = sc_logic_Z;
-		pin5 = sc_logic_Z;
-		pin6 = sc_logic_Z;
-		pin7 = sc_logic_Z;
-		pin8 = sc_logic_Z;
-		pin9 = sc_logic_Z;
-		pin11 = AS(IS_H(pin9));
-		pin12 = AS(IS_H(pin8));
-		pin13 = AS(IS_H(pin7));
-		pin14 = AS(IS_H(pin6));
-		pin15 = AS(IS_H(pin5));
-		pin16 = AS(IS_H(pin4));
-		pin17 = AS(IS_H(pin3));
-		pin18 = AS(IS_H(pin2));
-	}
+	state->ab = new SCM_F245_5("ab", &state->ctx.activations, true);
+	state->ba = new SCM_F245_5("ba", &state->ctx.activations, false);
+	state->ab->pin_oe(pin19);	state->ba->pin_oe(pin19);
+	state->ab->pin_dir(pin1);	state->ba->pin_dir(pin1);
+	state->ab->pin_d0(pin2);	state->ba->pin_y0(pin2);
+	state->ab->pin_d1(pin3);	state->ba->pin_y1(pin3);
+	state->ab->pin_d2(pin4);	state->ba->pin_y2(pin4);
+	state->ab->pin_d3(pin5);	state->ba->pin_y3(pin5);
+	state->ab->pin_d4(pin6);	state->ba->pin_y4(pin6);
+	state->ab->pin_d5(pin7);	state->ba->pin_y5(pin7);
+	state->ab->pin_d6(pin8);	state->ba->pin_y6(pin8);
+	state->ab->pin_d7(pin9);	state->ba->pin_y7(pin9);
+	state->ab->pin_y0(pin18);	state->ba->pin_d0(pin18);
+	state->ab->pin_y1(pin17);	state->ba->pin_d1(pin17);
+	state->ab->pin_y2(pin16);	state->ba->pin_d2(pin16);
+	state->ab->pin_y3(pin15);	state->ba->pin_d3(pin15);
+	state->ab->pin_y4(pin14);	state->ba->pin_d4(pin14);
+	state->ab->pin_y5(pin13);	state->ba->pin_d5(pin13);
+	state->ab->pin_y6(pin12);	state->ba->pin_d6(pin12);
+	state->ab->pin_y7(pin11);	state->ba->pin_d7(pin11);
 }
