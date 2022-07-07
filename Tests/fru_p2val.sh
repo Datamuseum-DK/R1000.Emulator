5ac4c4ac2aca87f51e50dc8639bebb64380f0dac
Poul-Henning Kamp <phk@FreeBSD.org>
2022-07-07 22:31:34 +0000

    More test-scripts

---
+++ Tests/fru_p2val.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+. Tests/subr_test.rc
+
+sc_boards ioc fiu mem0 seq typ val
+
+cli 'sc trace DI*PROC 4'
+cli 'sc trace LCNTR 1'
+cli 'sc quota add 5'
+cli 'sc quota exit'
+
+# 8 => Initialize processor state
+# 3 => Execute diagnostics
+# 4 => Run a specific test
+# 27 => P2VAL
+fru_prompt 8 3 4 27
+
+run
