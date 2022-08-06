5f823254da12842e7f2e3121d05c02a4fcbeb5a0
Poul-Henning Kamp <phk@FreeBSD.org>
2022-08-06 19:26:01 +0000

    P1SF test-case

---
+++ Tests/fru_p1sf.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+. Tests/subr_test.rc
+
+sc_boards ioc fiu mem0 seq typ val
+
+cli 'sc trace DI*PROC 4'
+cli 'sc quota add 5'
+cli 'sc quota exit'
+
+# 8 => Initialize processor state
+# 3 => Execute diagnostics
+# 4 => Run a specific test
+# 6 => P1SF
+# 1 => All boards
+fru_prompt 8 3 4 6 1
+
+run
