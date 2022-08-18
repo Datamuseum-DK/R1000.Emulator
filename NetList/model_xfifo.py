9d019ace45e4cfbe3e7a1806e4da862f6f446c19
Poul-Henning Kamp <phk@FreeBSD.org>
2022-08-18 07:35:07 +0000

    revert

--- NetList/model_xfifo.py
+++ NetList/model_xfifo.py
@@ -83,10 +83,9 @@ class XFIFO(PartFactory):
 		|			state->wptr &= 0xff;
 		|		}
 		|		if (PIN_RD.posedge()) {
-		|			state->rptr++;
+		|			state->oreg = state->ram[state->rptr++];
 		|			state->rptr &= 0xff;
 		|		}
-		|		state->oreg = state->ram[state->rptr];
 		|	}
 		|	if (!PIN_OEnot)
 		|		BUS_Q_WRITE(state->oreg);
