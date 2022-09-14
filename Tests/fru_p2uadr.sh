2c3680f279bdf6a872b8bfe952bb1d221b202c3d
Poul-Henning Kamp <phk@FreeBSD.org>
2022-09-14 13:40:28 +0000

    Add P2UADR script

---
+++ Tests/fru_p2uadr.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+. Tests/subr_test.rc
+
+sc_boards ioc fiu mem0 seq typ val
+
+cli 'sc trace DI*PROC 4'
+cli 'sc quota add 5'
+cli 'sc quota exit'
+
+cli 'dfs neuter TEST_WCS_LOCATIONS.TYP'
+
+# 8 => Initialize processor state
+# 3 => Execute diagnostics
+# 4 => Run a specific test
+# 25 => P2UADR
+fru_prompt 8 3 4 25
+
+run
