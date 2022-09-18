792b3bb2498e42885df216b5ad3ed2d43d65f75a
Poul-Henning Kamp <phk@FreeBSD.org>
2022-09-18 19:29:20 +0000

    Add a -quick mode

--- Tests/fru_p2ioc.sh
+++ Tests/fru_p2ioc.sh
@@ -2,6 +2,14 @@
 
 . Tests/subr_test.rc
 
+if [ "x$1" = "x-quick" ] ; then
+	cli 'dfs neuter TEST_WCS_BITS.IOC'
+	cli 'dfs neuter TEST_WCS_ADDRESSING.IOC'
+	cli 'dfs neuter TEST_TRACE_DATA.IOC'
+	cli 'dfs neuter TEST_TRACE_ADDRESSING.IOC'
+	cli 'dfs neuter TEST_TRACE_BITS.IOC'
+fi
+
 sc_boards ioc fiu mem0 seq typ val
 
 cli 'sc trace DI*PROC 4'
