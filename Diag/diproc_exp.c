c73163acc7ee528e3c3ec185209f28c044e30441
Poul-Henning Kamp <phk@FreeBSD.org>
2022-10-13 05:11:56 +0000

    SW load of TYP+VAL RF

--- Diag/diproc_exp.c
+++ Diag/diproc_exp.c
@@ -33,6 +33,8 @@ struct diagproc_exp_priv {
 	unsigned	counter;
 	uint8_t		*ram;
 	uint8_t		*ip;
+	uint8_t		*dst1;
+	uint8_t		*dst2;
 	board_func_t	*board;
 };
 
@@ -214,6 +216,76 @@ seq_board(struct diagproc_exp_priv *dep, uint8_t length)
 	return (0);
 }
 
+static uint64_t
+get_wdr(struct diagproc_exp_priv *dep, uint8_t offset)
+{
+	uint64_t wdr, m;
+	unsigned v;
+	int i;
+
+	//printf("%s %02x: ", dep->name, offset);
+	wdr = 0;
+	m = 1ULL << 63;
+	//for (i = 0; i < 12; i++)
+	//	printf("%02x ", dep->ram[offset + i]);
+	for (v = 0x80; v > 0x02; v >>= 1) {
+		for (i = 11; i >= 0; i--) {
+			if (v == 0x20 && i < 4)
+				continue; // parity
+			if (v == 0x10 && i >= 8)
+				continue; // parity
+			if (dep->ram[offset + i] & v) {
+				//putchar('#');
+				wdr |= m;
+			} else {
+				//putchar('-');
+			}
+			m >>= 1;
+		}
+	}
+	//printf("=> %016jx\n", (uintmax_t)wdr);
+	return (wdr);
+}
+
+static int
+typ_val_rfload(struct diagproc_exp_priv *dep, uint8_t length, const char *aram, const char *bram, const char *suf)
+{
+	uint64_t wdr;
+	unsigned w;
+	struct ctx *ctx;
+	uint8_t *dst;
+
+	if (dep->ram[0x10] != 0xd9)
+		return(0);
+
+	if (length == 0xdc) {
+		// LOAD_REGISTER_FILE_200.TYP (complete download)
+		ctx = CTX_Find(aram);
+		AN(ctx);
+		dst = (uint8_t*)(ctx + 1);
+		dep->dst1 = dst;
+
+		ctx = CTX_Find(bram);
+		AN(ctx);
+		dst = (uint8_t*)(ctx + 1);
+		dep->dst2 = dst;
+	}
+	if (length == 0xdc || length == 0xc8) {
+		// LOAD_REGISTER_FILE_200.TYP (complete & modify downloads)
+		sc_tracef(dep->name, "Exp load_register_file_200.%s", suf);
+		for (w = 0x18; w < 0xd8; w += 12) {
+			wdr = get_wdr(dep, w);
+			memcpy(dep->dst1, &wdr, sizeof wdr);
+			dep->dst1 += 8;
+			memcpy(dep->dst2, &wdr, sizeof wdr);
+			dep->dst2 += 8;
+			fflush(stdout);
+		}
+		return (1);
+	}
+	return (0);
+}
+
 static int
 typ_board(struct diagproc_exp_priv *dep, uint8_t length)
 {
@@ -224,18 +296,27 @@ typ_board(struct diagproc_exp_priv *dep, uint8_t length)
 		SET_PT(dep);
 		return (1);
 	}
+
+	if (typ_val_rfload(dep, length, "TYP.typ_16.ARAM", "TYP.typ_18.BRAM", "typ"))
+		return (1);
+
 	return (0);
 }
 
 static int
 val_board(struct diagproc_exp_priv *dep, uint8_t length)
 {
+
 	if (length == 0x40 && dep->ram[0x10] == 0x26) {
 		sc_tracef(dep->name, "Exp read_novram_data.val");
 		upload(dep->ram + 0x19, "2a28060103b796");
 		SET_PT(dep);
 		return (1);
 	}
+
+	if (typ_val_rfload(dep, length, "VAL.val_18.ARAM", "VAL.val_20.BRAM", "val"))
+		return (1);
+
 	return (0);
 }
 
