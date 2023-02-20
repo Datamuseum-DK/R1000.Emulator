919014d23e3c57c886543bfc1939e56e2a0e8a01
Poul-Henning Kamp <phk@FreeBSD.org>
2023-02-20 09:20:04 +0000

    Give boot_env.sh an optional `--background` argument

--- Tests/boot_env.sh
+++ Tests/boot_env.sh
@@ -4,13 +4,22 @@ set -e
 
 . Tests/subr_test.rc
 
+if [ "x$1" == "x--background" ] ; then
+	BACKGROUND=true
+	shift
+else
+	BACKGROUND=false
+fi
+
 if [ "x$1" == "x" ] ; then
+	runname=boot
 	rundir=${R1K_WORKDIR}/boot
 	rm -rf ${rundir}
 elif [ -d ${R1K_WORKDIR}/$1 ] ; then
 	echo "Run-name (${R1K_WORKDIR}/$1) directory already exists" 1>&2
 	exit 1
 else
+	runname=$1
 	rundir=${R1K_WORKDIR}/$1
 fi
 
@@ -84,6 +93,14 @@ cli 'console << ""'
 cli 'console match expect "Enter option [enter CLI] : "'
 cli 'console << "6"'
 
-./r1000sim \
-	-T ${rundir}/_r1000 \
-	"include ${R1K_CLIFILE}" 2>&1 | tee ${rundir}/_r1000.log
+if $BACKGROUND ; then
+	cli 'console match expect "notevermatching"'
+	nohup ./r1000sim \
+		-T ${rundir}/_r1000 \
+		"include ${R1K_CLIFILE}" 2>&1 | tee ${rundir}/_r1000.log \
+		2>&1 > _.run &
+else
+	./r1000sim \
+		-T ${rundir}/_r1000 \
+		"include ${R1K_CLIFILE}" 2>&1 | tee ${rundir}/_r1000.log
+fi
