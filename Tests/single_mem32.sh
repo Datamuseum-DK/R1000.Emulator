2e69abe2e63ae77f528882ad8f13fd49f08097c5
Poul-Henning Kamp <phk@FreeBSD.org>
2023-03-30 10:45:16 +0000

    Polish

--- Tests/single_mem32.sh
+++ Tests/single_mem32.sh
@@ -4,13 +4,7 @@
 
 sc_boards mem0
 
-cli 'sc trace mem32_32 1'
-cli 'sc trace DINAN0A 0'
-cli 'sc trace mem32_32.DI*PROC 5'
-cli 'sc trace mem32_33 1'
-cli 'sc trace DIAOI0B_1 0'
-cli 'sc trace DINOR0B 0'
-cli 'sc trace DISTPAL 0'
-cli 'dfs patch LOAD_CONFIG.M32 0x3c 0x30 0x32'
+cli 'sc trace mem32_33.DFSM 1'
+#cli 'dfs patch LOAD_CONFIG.M32 0x3c 0x30 0x32'
 
 single mem0 $*
